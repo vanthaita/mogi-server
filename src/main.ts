b4913ff86ef8368ccbc54f2752c8bb2e52ad773c
vanthaita <thaitv225@gmail.com>
2024-09-18 18:17:13 +0700

    fix: config main

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    {
+      cors: true,
+    },
+  );
+
   app.use(cookieParser());
-  app.enableCors({
-    origin: process.env.NEXT_PUBLIC_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: process.env.NEXT_PUBLIC_URL,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
 
   const port = process.env.PORT;
   await app.listen(port);

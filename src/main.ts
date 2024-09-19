a8528319627f58d47d0243333b5a58b9465560b9
vanthaita <thaitv225@gmail.com>
2024-09-19 14:47:15 +0700

    fix: config main

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+  );
+
+  const configService = app.get(ConfigService);
+  const COOKIE_SECRET = configService.get<string>('COOKIE_SECRET');
+  const PORT = configService.get<string>('PORT') || 8386;
+  const HOST = configService.get<string>('HOST') || '0.0.0.0';
+  const FRONTEND_URL =
+    configService.get<string>('NEXT_PUBLIC_URL') ||
+    'https://mogi-three.vercel.app';
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.NEXT_PUBLIC_URL], // List the allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Enable credentials for cookie-based auth
+    origin: FRONTEND_URL,
+    credentials: true,
   });
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
+  app.use(cookieParser(COOKIE_SECRET));
+  await app.listen(PORT, HOST);
+  console.log(`Server running at http://${HOST}:${PORT}`);
 }
 
 bootstrap();
